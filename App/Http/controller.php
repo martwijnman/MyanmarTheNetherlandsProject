53ed04dac793d06a6c57374833ec5eb4738f9f5a
martwijnman <153086626+martwijnman@users.noreply.github.com>
2024-09-28 13:12:16 +0200

    Update controller.php
    I've improve the login system

--- App/Http/controller.php
+++ App/Http/controller.php
@@ -1,5 +1,26 @@
 <?php 
 $action = $_POST['action'];
+if($action == "register"){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+}
+if($action == "login"){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM users WHERE username = :username";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([
+            ":username" => $username,
+    ]);
+    $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $_SESSION['password'] = $user['password'];
+    $hash = password_hash($password);
+    if(password_verify($SESSION['password'], $hash)){
+        session_start();
+        header('homepage.php');
+    }
+    
+}
 if($action == "create"){
     $example = $_POST['example']; // We need to discuss which elements we should use
     $sql = "INSERT INTO dbs(example) VALUES(:example)";
