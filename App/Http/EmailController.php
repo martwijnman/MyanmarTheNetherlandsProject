c84911bf012afc48db419fbf083929d1b3221e9d
martwijnman <153086626+martwijnman@users.noreply.github.com>
2024-09-29 14:59:43 +0200

    organise controllers

---
+++ App/Http/EmailController.php
@@ -0,0 +1,13 @@
+<?php
+$action = $_POST['action'];
+if($action == "email"){
+    $email = $_POST['email'];
+    $content = $_POST['content'];
+    $sql = "INSERT INTO emails(email, content) VALUES(:email, :content)";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([
+            ":email" => $email,
+            ":content" => $content,
+    ]);
+    $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
---
+++ App/Http/PostController.php
@@ -0,0 +1,38 @@
+<?php 
+$action = $_POST['action'];
+if($action == "register"){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+}
+// if($action == "login"){
+//     $username = $_POST['username'];
+//     $password = $_POST['password'];
+//     $sql = "SELECT * FROM users WHERE username = :username";
+//     $stmt = $conn->prepare($sql);
+//     $stmt->execute([
+//             ":username" => $username,
+//     ]);
+//     $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//     $_SESSION['password'] = $user['password'];
+//     $hash = password_hash($password);
+//     if(password_verify($SESSION['password'], $hash)){
+//         session_start();
+//         header('homepage.php');
+//     }
+    
+// }
+if($action == "create"){
+    $example = $_POST['example']; // We need to discuss which elements we should use
+    $sql = "INSERT INTO dbs(example, example, example, example, example, example) VALUES(:example, :example, :example, :example, :example, :example)"; // todo fill the data
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([
+            ":example" => $example,
+            ":example" => $example,
+            ":example" => $example,
+            ":example" => $example,
+            ":example" => $example,
+            ":example" => $example,
+    ]);
+    $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
